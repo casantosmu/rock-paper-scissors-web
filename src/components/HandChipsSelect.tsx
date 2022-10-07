67abf0e32a8faa94776c222824b0fd544c67c80a
Carlos <casantosmu@gmail.com>
2022-10-07 06:34:04 +0200

    Added HandChipsResults and HandChipsSelect

---
+++ src/components/HandChipsSelect.tsx
@@ -0,0 +1,18 @@
+import { HandNames } from "../types/interfaces";
+import { HandChip } from "./HandChip";
+
+interface HandChipSelectProps {
+  hands: HandNames[];
+}
+
+const HandChipSelect = ({ hands }: HandChipSelectProps): JSX.Element => {
+  return (
+    <>
+      {hands.map((hand) => {
+        return <HandChip name={hand} isButton />;
+      })}
+    </>
+  );
+};
+
+export default HandChipSelect;
