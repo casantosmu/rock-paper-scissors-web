1caef27c4590c6ad3b5853aef78b10c4b24c327a
Carlos <casantosmu@gmail.com>
2022-10-13 17:39:13 +0200

    Modified Header to add width and height atributes on logo image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,13 @@ const Header = (): JSX.Element => {
   const { roomId } = useContext(RoomContext);
 
   return (
-    <header className="flex justify-between items-center p-2 rounded-md border-4	border-neutral-50/30">
-      <img src={logoImage} alt="Rock, Paper, Scissors logo" />
+    <header className="flex justify-between items-center p-4 rounded-lg border-3 border-neutral-50/30">
+      <img
+        src={logoImage}
+        alt="Rock, Paper, Scissors logo"
+        width={162}
+        height={99}
+      />
       {roomId && <ScoreDisplay />}
     </header>
   );
