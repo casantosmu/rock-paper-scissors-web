00b6ece1dfc2ecd91606061f54c0211fb7152c30
Carlos <casantosmu@gmail.com>
2022-10-13 17:04:21 +0200

    Modified SharedLayout to add padding and gap between elements and width

--- src/layouts/SharedLayout.tsx
+++ src/layouts/SharedLayout.tsx
@@ -4,9 +4,9 @@ import Header from "../components/Header";
 const SharedLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="bg-blue-900">
-      <div className="flex flex-col min-h-screen w-full max-w-lg m-auto ">
+      <div className="flex flex-col gap-20 min-h-screen max-w-lg m-auto pt-10 w-11/12">
         <Header />
-        {children}
+        <main>{children}</main>
       </div>
     </div>
   );
