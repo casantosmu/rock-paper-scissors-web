60fb30b7517ebdbfb96a491b2c6c8cc22ae2919b
Carlos <casantosmu@gmail.com>
2022-10-21 21:45:19 +0200

    Removed RoomContext and RoomConectProvider

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import RoomContextProvier from "./store/Room/context/RoomContextProvider";
 import MoveContextProvider from "./store/Move/context/MoveContextProvider";
 
 const root = ReactDOM.createRoot(
@@ -10,10 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <RoomContextProvier>
-      <MoveContextProvider>
-        <App />
-      </MoveContextProvider>
-    </RoomContextProvier>
+    <MoveContextProvider>
+      <App />
+    </MoveContextProvider>
   </React.StrictMode>
 );
