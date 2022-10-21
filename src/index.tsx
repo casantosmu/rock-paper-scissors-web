e99a03040cd3885b0ea37557c6cf1c9ee2289eac
Carlos <casantosmu@gmail.com>
2022-10-21 22:33:36 +0200

    Removed RoomContext & Modified Chip components to recive state by props

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import MoveContextProvider from "./store/Move/context/MoveContextProvider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <MoveContextProvider>
-      <App />
-    </MoveContextProvider>
+    <App />
   </React.StrictMode>
 );
