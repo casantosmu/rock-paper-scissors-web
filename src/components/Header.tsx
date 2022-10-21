6ddf2b7703d6d140f4814856be623b4df4f36789
Carlos <casantosmu@gmail.com>
2022-10-21 21:50:13 +0200

    Modified Header, JoinRoom and Score to recive dispatch/state by props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
-import { useContext } from "react";
 import logoImage from "../assets/logo.svg";
-import RoomContext from "../store/Room/context/RoomContext";
-import ScoreDisplay from "./ScoreDisplay";
 
-const Header = (): JSX.Element => {
-  const { roomId } = useContext(RoomContext);
+interface HeaderProps {
+  scoreDisplay?: JSX.Element;
+}
 
+const Header = ({ scoreDisplay }: HeaderProps): JSX.Element => {
   return (
     <header className="flex justify-between items-center p-4 rounded-lg border-3 border-neutral-50/30">
       <img
@@ -14,7 +13,7 @@ const Header = (): JSX.Element => {
         width={162}
         height={99}
       />
-      {roomId && <ScoreDisplay />}
+      {scoreDisplay}
     </header>
   );
 };
